41ef4151e9ee3b553e840d8820baeb364134f6fe
Richard Cheung <richard.yh.cheung@gmail.com>
2022-04-09 17:06:25 -0700

    Added distances and fixed card

--- src/MainScreen.tsx
+++ src/MainScreen.tsx
@@ -1,5 +1,7 @@
+import { Container } from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
-import Card from "./components/Card";
+import HospitalCard from "./components/Card";
 import { useHospitalDataQuery } from "./utils/updates";
 
 type Props = {};
@@ -13,13 +15,39 @@ const MainScreen = (props: Props) => {
 
   return (
     <div>
-      <div></div>
-      <h2>{hospitalData.apiUpdateTime}</h2>
-      <div>
-        {hospitalData.waitTimes.map((hospital) => (
-          <Card data={hospital} />
-        ))}
-      </div>
+      <Container>
+        {/* Replace with Typography */}
+        <Box
+          sx={{
+            fontSize: 35,
+            fontWeight: "bold",
+          }}
+        >
+          HK Hospitals
+        </Box>
+        <Box
+          sx={{
+            fontSize: 17,
+            color: "#6D6D6D",
+          }}
+        >
+          Accident & Emergency Wait Times
+        </Box>
+        <Box
+          sx={{
+            fontSize: 14,
+            color: "#6D6D6D",
+            fontWeight: 200,
+          }}
+        >
+          Last update: {hospitalData.apiUpdateTime}
+        </Box>
+        <div>
+          {hospitalData.waitTimes.map((hospital) => (
+            <HospitalCard data={hospital} />
+          ))}
+        </div>
+      </Container>
     </div>
   );
 };
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,29 @@
+import { Card, CardContent, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
 import { Hospital } from "../../shared/types";
+import MapIcon from "@mui/icons-material/Map";
 
 type Props = {
   data: Hospital;
 };
 
-export default function Card({ data }: Props) {
+export default function HospitalCard({ data }: Props) {
   return (
-    <div>
-      <h1>{data.name[0]}</h1>
-      <h3>{data.waitTimeText}</h3>
-      <p>{data.address[0]}</p>
-    </div>
+    <Card variant="elevation" sx={{ borderRadius: 4, mt: 4 }}>
+      <CardContent>
+        <Box display="flex" justifyContent="space-between">
+          <Box>
+            <Typography>{data.name[0]}</Typography>
+            <Box display="flex">
+              <MapIcon fontSize="medium" />
+              <Typography>4.9km</Typography>
+              <Typography>New Territories</Typography>
+            </Box>
+          </Box>
+          <Typography>{data.waitTimeValue}</Typography>
+        </Box>
+      </CardContent>
+    </Card>
   );
 }
