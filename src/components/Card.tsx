d57a723a824ff934db29f0ca7ec6687d1f88c8c7
Richard Cheung <richard.yh.cheung@gmail.com>
2022-04-09 17:58:52 -0700

    implemented ring

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,18 +1,54 @@
 import React from "react";
+import { MdMap } from "react-icons/md";
+import styled from "styled-components";
 import { Hospital } from "../../shared/types";
+import Ring from "./Ring";
 
 type Props = {
   data: Hospital;
 };
 
+const Card = styled.div`
+  box-shadow: 1px 2px 7px rgba(0, 0, 0, 0.25);
+  border-radius: 18px;
+  margin-bottom: 24px;
+  padding: 15px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const LocationInfoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 8px;
+`;
+
+const HospitalName = styled.h2`
+  font-size: 1.2em;
+`;
+
+const Distance = styled.p`
+  font-size: 1em;
+  margin: 0 7px;
+`;
+
+const District = styled.p`
+  font-size: 0.9em;
+`;
+
 export default function HospitalCard({ data }: Props) {
   return (
-    <div>
-      <h1>{data.name[0]}</h1>
-      {/* <MapIcon fontSize="medium" /> */}
-      <h2>4.9km</h2>
-      <h3>New Territories</h3>
-      <h4>{data.waitTimeValue}</h4>
-    </div>
+    <Card>
+      <div>
+        <HospitalName>{data.name[0]}</HospitalName>
+        <LocationInfoWrapper>
+          <MdMap size={20} />
+          <Distance>4.9km</Distance>
+          <District>New Territories</District>
+        </LocationInfoWrapper>
+      </div>
+      <Ring time={data.waitTimeValue} />
+    </Card>
   );
 }
---
+++ src/components/Ring.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import styled from "styled-components";
+
+const getRingColor = (time: number) => {
+  if (time < 3) {
+    return "#3DBD00";
+  } else if (time < 5) {
+    return "#D09600";
+  }
+  return "#BD0000";
+};
+
+const RingContainer = styled.div`
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  border: ${(props) => props.color} 7px solid;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  line-height: 1.3em;
+`;
+
+const WaitTimeValue = styled.h1`
+  font-size: 1.3em;
+  margin-bottom: -3px;
+`;
+
+const HoursText = styled.p``;
+
+type Props = {
+  time: number;
+};
+
+const Ring = ({ time }: Props) => {
+  return (
+    <div>
+      <RingContainer color={getRingColor(time)}>
+        <WaitTimeValue>{time}</WaitTimeValue>
+        <HoursText>hr{time > 1 ? "s" : ""}</HoursText>
+      </RingContainer>
+    </div>
+  );
+};
+
+export default Ring;
